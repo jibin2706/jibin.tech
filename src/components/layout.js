57f67e9777288bc3c7fd7ca1671b86048d5c170c
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-03-22 17:16:33 +0530

    Removed unnecessary files from the default starter

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,41 +13,15 @@ import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+  <>
+    <Header />
+    <div>
+      <main>{children}</main>
+      <footer>
+        <p>footer</p>
+      </footer>
+    </div>
+  </>
 )
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
 
 export default Layout
