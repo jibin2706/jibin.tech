bc8b317b74cb5e97dc5e64bf76d4967bfcc05a47
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-03-22 23:49:10 +0530

    Styled footer

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      <p>Powered by Gatsby.js</p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,25 +1,15 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
+import React from 'react'
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
+import Footer from './footer'
+import './layout.css'
 
 const Layout = ({ children }) => (
   <>
     <Header />
     <div>
       <main>{children}</main>
-      <footer>
-        <p>footer</p>
-      </footer>
+      <Footer />
     </div>
   </>
 )
