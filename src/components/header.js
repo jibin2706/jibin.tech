6f87b970d623f96f5c6b64946c2810c80a802310
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-03-24 11:58:30 +0530

    Added header link to go to the home page

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import ToggleDarkButton from './toggleViewButton'
 
 import './header.scss'
 
 const Header = () => (
   <header id="header">
-    <h1 className="title">header</h1>
+    <Link to="/">
+      <h1 className="title">header</h1>
+    </Link>
     <span className="toggle-button">
       <ToggleDarkButton />
     </span>
