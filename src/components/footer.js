0419c8e66b0045f769400a5690db2b95c3eddc11
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-04-26 19:00:57 +0530

    Added social media svgs instead of text

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import instagram from '../images/4372354761555590083.svg'
+import github from '../images/github.svg'
+import rss from '../images/rss.svg'
+import twitter from '../images/twitter.svg'
+import linkedin from '../images/linkedin.svg'
 
 import './footer.scss'
 
@@ -7,12 +12,21 @@ function Footer() {
     <footer>
       <hr style={{ maxWidth: '800px', margin: '16px auto' }} />
       <div className="social">
-        <a href="https://twitter.com/real_jibin">Twitter</a>
-        <a href="https://www.linkedin.com/in/jibin-thomas-4b3496142/">
-          LinkedIn
+        <a href="https://twitter.com/real_jibin">
+          <img src={twitter} alt="twitter" />
         </a>
-        <a href="https://www.instagram.com/jibin2706/">Instagram</a>
-        <a href="https://github.com/jibin2706">Github</a>
+        <a href="https://www.linkedin.com/in/jibin-thomas-4b3496142/">
+          <img src={linkedin} alt="linkedin" />
+        </a>
+        <a href="https://www.instagram.com/jibin2706/">
+          <img src={instagram} alt="instagram" />
+        </a>
+        <a href="https://github.com/jibin2706">
+          <img src={github} alt="github" />
+        </a>
+        <a href="./rss.xml">
+          <img src={rss} alt="rss" />
+        </a>
       </div>
       <div className="footer-info">
         <p>© - Coded by hand and headaches</p>
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,9 @@ import Header from './header'
 import Footer from './footer'
 
 const Layout = ({ children }) => (
-  <div style={{ maxWidth: '680px', margin: 'auto', padding: '16px' }}>
+  <div
+    style={{ maxWidth: '680px', margin: 'auto', padding: '16px 16px 0 16px' }}
+  >
     <Header />
     <div>
       <div>{children}</div>
