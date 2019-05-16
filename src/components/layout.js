b8ae064083f5e0e9a3607ca753688f457786c206
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-05-16 07:30:28 +0530

    Updated layout.js file

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,13 @@
 import React from 'react'
 
-import Header from './header'
 import Footer from './footer'
 
-import './layout.css'
-import './style.scss'
-
 const Layout = ({ children }) => (
-  <div style={{ maxWidth: '680px', margin: 'auto', padding: '16px' }}>
-    <Header />
-    <div>
-      <main>{children}</main>
-      <Footer />
-    </div>
+  <div
+    style={{ maxWidth: '680px', margin: 'auto', padding: '16px 16px 0 16px' }}
+  >
+    <div>{children}</div>
+    <Footer />
   </div>
 )
 
