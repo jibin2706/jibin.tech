7bd28b8b146b6970fda679517d77b6a926aaaae5
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-05-27 20:15:55 +0530

    Removed typography.js package

--- gatsby-config.js
+++ gatsby-config.js
@@ -142,13 +142,6 @@ module.exports = {
         ],
       },
     },
-    // typography plugin
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     'gatsby-plugin-offline',
