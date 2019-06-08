554a500b882ab4a872cfff4c0a63be8316f5d31a
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-06-08 17:10:25 +0530

    Changed footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,4 @@
 import React from 'react'
-import instagram from '../images/instagram.svg'
-import github from '../images/github.svg'
-import rss from '../images/rss.svg'
-import twitter from '../images/twitter.svg'
 
 import './footer.scss'
 
@@ -11,18 +7,12 @@ function Footer() {
     <footer>
       <hr style={{ maxWidth: '800px', margin: '16px auto' }} />
       <div className="social">
-        <a href="https://twitter.com/real_jibin">
-          <img src={twitter} alt="follow me on twitter" />
+        <a href="https://github.com/jibin2706">Github</a>
+        <a href="https://twitter.com/real_jibin">Twitter</a>
+        <a href="https://www.linkedin.com/in/jibin-thomas-4b3496142/">
+          LinkedIn
         </a>
-        <a href="https://www.instagram.com/jibin2706/">
-          <img src={instagram} alt="instagram" />
-        </a>
-        <a href="https://github.com/jibin2706">
-          <img src={github} alt="github" />
-        </a>
-        <a href="https://blog.jibin.tech/rss.xml">
-          <img src={rss} alt="rss" />
-        </a>
+        <a href="https://blog.jibin.tech/rss.xml">RSS feed</a>
       </div>
       <div className="footer-info">
         <p>© - Coded by hand and headaches</p>
