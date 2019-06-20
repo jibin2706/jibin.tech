a8daa9b738864f298c678e75e12a3660ceda7c93
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-06-20 10:55:10 +0530

    Updated href

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,21 +4,26 @@ import './footer.scss'
 
 function Footer() {
   return (
-    <footer>
-      <hr style={{ maxWidth: '800px', margin: '16px auto' }} />
-      <div className="social">
-        <a href="https://github.com/jibin2706">Github</a>
-        <a href="https://twitter.com/real_jibin">Twitter</a>
-        <a href="https://www.linkedin.com/in/jibin-thomas-4b3496142/">
-          LinkedIn
-        </a>
-        <a href="https://blog.jibin.tech/rss.xml">RSS feed</a>
-      </div>
-      <div className="footer-info">
-        <p>© - Coded by hand and headaches</p>
-        <p>Powered by Gatsby.js</p>
-      </div>
-    </footer>
+    <>
+      <footer>
+        <section className="social">
+          <a href="https://github.com/jibin2706">Github</a>
+          <a href="https://twitter.com/real_jibin">Twitter</a>
+          <a href="https://www.linkedin.com/in/jibin2706/">LinkedIn</a>
+          <a href="https://blog.jibin.tech/rss.xml">RSS feed</a>
+        </section>
+
+        <div className="footer-info">
+          <p>Coded by hand and headaches</p>
+          <p class="footer-end">
+            This site is built with{' '}
+            <a href="https://gatsbyjs.org/">Gatsby.js </a>. The source code is
+            hosted on{' '}
+            <a href="https://github.com/jibin2706/blog.jibin.tech">Github</a>.
+          </p>
+        </div>
+      </footer>
+    </>
   )
 }
 
