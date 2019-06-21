d9833ccd0c0e527b20ff2ce9e96570986818a5c3
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-06-21 16:14:52 +0530

    Added contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+import '../components/layout.css'
+import '../components/globals.scss'
+
+import ContactForm from '../components/ContactForm'
+
+const Contact = () => (
+  <Layout>
+    <SEO title="Contact Jibin Thomas - Front-End Developer and a Causal Blogger" />
+    <div style={{ maxWidth: '600px' }}>
+      <h1>Hi👋, it’s nice to meet you.</h1>
+      <p>
+        You have any projects that I could help with? Then feel free to reach
+        out to me by filling the form. I’ll get back to you quickly.
+      </p>
+
+      <ContactForm />
+    </div>
+  </Layout>
+)
+
+export default Contact
