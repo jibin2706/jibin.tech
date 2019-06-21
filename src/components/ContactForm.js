06af7f11b61372d55efc9410c890facd32d772ef
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-06-21 22:01:52 +0530

    Configured contact form with netlify

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,25 +4,42 @@ import './contact_form.scss'
 
 function Contact() {
   return (
-    <form className="contact-form">
+    <form
+      className="contact-form"
+      method="POST"
+      netlify-honeypot="bot-field"
+      data-netlify="true"
+      action="/success"
+    >
+      <p class="hidden">
+        <label>
+          Don’t fill this out if you're human: <input name="bot-field" />
+        </label>
+      </p>
       <label>
         <span className="contact-form__label">Full Name</span>
-        <input type="text" />
+        <input type="text" name="name" required />
       </label>
 
       <label>
         <span className="contact-form__label">Email Address</span>
-        <input type="email" />
+        <input type="email" name="email" required />
       </label>
 
       <label>
         <span className="contact-form__label">Company / Organization</span>
-        <input type="text" />
+        <input type="text" name="company_name" />
       </label>
 
       <label>
         <span className="contact-form__label">Message</span>
-        <textarea row="20" cols="50" placeholder="Your message" />
+        <textarea
+          row="20"
+          cols="50"
+          placeholder="Your message"
+          name="message"
+          required
+        />
       </label>
 
       <button className="btn">Send Email</button>
