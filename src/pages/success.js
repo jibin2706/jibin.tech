f28f2d2af0c421d7e3ecc7763a3fdc23e3810ea9
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-06-21 22:02:19 +0530

    Created success page

---
+++ src/pages/success.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SuccessMessage from '../components/SuccessMessage'
+
+function Success() {
+  return (
+    <Layout>
+      <SuccessMessage />
+    </Layout>
+  )
+}
+
+export default Success
