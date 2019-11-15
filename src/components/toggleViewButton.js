abe67dfa88bd9e647f15489e80fd9590a9930e94
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-11-16 00:17:30 +0530

    Adjusted header styles

--- src/components/toggleViewButton.js
+++ src/components/toggleViewButton.js
@@ -25,6 +25,7 @@ function ToggleThemeButton() {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill={darkMode.value ? '#f5f7fa' : '#282c35'}
+        width="18"
       >
         <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10S2 17.514 2 12 6.486 2 12 2zm0-2C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm0 4a8 8 0 000 16V4z" />
       </svg>
