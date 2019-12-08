ecfa1ab8e880359e51b52ddabf26e46491345f6a
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-12-08 19:29:17 +0530

    Added svg to blogPost meta data

--- src/templates/blogPostTemplate.js
+++ src/templates/blogPostTemplate.js
@@ -53,8 +53,26 @@ export default ({ data, pageContext }) => {
         <header>
           <h1 className="blog__title">{title}</h1>
           <div className="blog__meta">
-            <span> {date}</span>
-            <span> {timeToRead} mins read</span>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+              >
+                <path d="M20 20h-4v-4h4v4zm-6-10h-4v4h4v-4zm6 0h-4v4h4v-4zM8 16H4v4h4v-4zm6 0h-4v4h4v-4zm-6-6H4v4h4v-4zm16-8v22H0V2h3v1c0 1.103.897 2 2 2s2-.897 2-2V2h10v1c0 1.103.897 2 2 2s2-.897 2-2V2h3zm-2 6H2v14h20V8zm-2-7a1 1 0 10-2 0v2a1 1 0 102 0V1zM6 3a1 1 0 11-2 0V1a1 1 0 112 0v2z"></path>
+              </svg>
+              {date}{' '}
+            </span>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+              >
+                <path d="M23 5v13.883L22 19V3c-3.895.119-7.505.762-10.002 2.316C9.502 3.762 5.896 3.119 2 3v16l-1-.117V5H0v15h9.057c1.479 0 1.641 1 2.941 1 1.304 0 1.461-1 2.942-1H24V5h-1zM11 18.645c-1.946-.772-4.137-1.269-7-1.484V5.11c2.352.197 4.996.675 7 1.922v11.613zm9-1.484c-2.863.215-5.054.712-7 1.484V7.032c2.004-1.247 4.648-1.725 7-1.922v12.051z"></path>
+              </svg>{' '}
+              {timeToRead} mins read
+            </span>
           </div>
           <h2 className="blog__info">{info}</h2>
           <div className="blog__tags">
