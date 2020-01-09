092e2d4006c2c476aa182603f548b034c5ccd32c
Jibin Thomas <jibin.thomas2706@gmail.com>
2020-01-09 23:35:57 +0530

    Increased color contrast & font-size

--- src/components/portfolio/LatestPosts.js
+++ src/components/portfolio/LatestPosts.js
@@ -9,47 +9,39 @@ function LatestPosts() {
       <h2 className="portfolio__header">Latest Posts</h2>
       <h3 className="portfolio__sub-header">I write some stuff that I learn</h3>
       <div className="blog__container">
+        <PostTemplate title="2019 In Review" url="/2019-in-review/" date="Dec 30, 2019" />
+
+        <PostTemplate title="Dark mode for the web" url="/detecting-dark-mode-in-your-web-apps/" date="Nov 24, 2019" />
+
         <PostTemplate
           title="Destructuring javascript objects into custom variables names and default values"
           url="/destructuring-assignment-of-objects-in-javascipt/"
-          date="19th October, 2019"
+          date="Oct 19, 2019"
         />
 
         <PostTemplate
           title="Getting started with Git - Part 1"
           url="/getting-started-with-git-part-1/"
-          date="21th September, 2019"
+          date="Sept 21, 2019"
         />
 
         <PostTemplate
           title="Different ways to get your react project started"
           url="/ways-to-get-your-react-project-started/"
-          date="12th September, 2019"
+          date="Sept 12, 2019"
         />
 
         <PostTemplate
           title="Difference between forEach and map in javascript"
           url="/difference-between-forEach-and-map-in-javascript/"
-          date="24th August, 2019"
+          date="Aug 24, 2019"
         />
 
-        <PostTemplate title="Next.js + Redux" url="/nextjs+redux/" date="11th August, 2019" />
-
-        <PostTemplate
-          title="Using locally hosted fonts on your website"
-          url="/hosting-font-locally-for-your-website/"
-          date="7th July, 2019"
-        />
-
-        <PostTemplate
-          title="Nifty Javascript methods that will make your life easier"
-          url="/nifty-javascript-methods-that-will-make-your-life-easier/"
-          date="2nd July, 2019"
-        />
+        <PostTemplate title="Next.js + Redux" url="/nextjs+redux/" date="Aug 11, 2019" />
 
         <p>
           Read more such
-          <Link href="/blog"> post here</Link>
+          <Link to="/blog"> post here</Link>
         </p>
       </div>
     </section>
