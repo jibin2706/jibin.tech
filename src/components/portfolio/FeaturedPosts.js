ec56774db52fbcc93e9e159c8b2d5b3a8f2678c1
Jibin Thomas <jibin.thomas2706@gmail.com>
2020-04-24 00:24:19 +0530

    Add project description and fixed styling on homepage

--- src/components/portfolio/FeaturedPosts.js
+++ src/components/portfolio/FeaturedPosts.js
@@ -7,7 +7,7 @@ import './posts.scss'
 function FeaturedPosts() {
   const data = useStaticQuery(graphql`
     query {
-      allMdx(filter: { frontmatter: { featured: { eq: true } } }) {
+      allMdx(filter: { frontmatter: { featured: { eq: true } } }, sort: { fields: [frontmatter___date], order: DESC }) {
         edges {
           node {
             id
@@ -28,19 +28,17 @@ function FeaturedPosts() {
     <section className="blog">
       <h2 className="portfolio__header">Featured Posts</h2>
       <h3 className="portfolio__sub-header">I write about stuff that I learn</h3>
-      <div className="blog__container">
-        {data.allMdx.edges.map(post => (
-          <Link key={post.node.id} to={post.node.fields.slug} className="post">
-            <h3 className="post__title">{post.node.frontmatter.title}</h3>
-            <p style={{ margin: 0 }}>{post.node.frontmatter.info}</p>
-          </Link>
-        ))}
+      {data.allMdx.edges.map((post) => (
+        <Link key={post.node.id} to={post.node.fields.slug} className="post">
+          <h3 className="post__title">{post.node.frontmatter.title}</h3>
+          <p className="post__description">{post.node.frontmatter.info}</p>
+        </Link>
+      ))}
 
-        <p>
-          Read more such
-          <Link to="/blog/"> post here</Link>
-        </p>
-      </div>
+      <p>
+        Read more such
+        <Link to="/blog/"> post here</Link>
+      </p>
     </section>
   )
 }
--- src/components/portfolio/Projects.js
+++ src/components/portfolio/Projects.js
@@ -25,11 +25,15 @@ function Projects() {
       <h2 className="portfolio__header">Projects</h2>
       <h3 className="portfolio__sub-header">Some fun projects that I have developed</h3>
 
-      {data.allProjectsJson.edges.map(project => (
+      {data.allProjectsJson.edges.map((project) => (
         <div className="project-container" key={project.node.title}>
           <h3 className="project-header">{project.node.title}</h3>
-          <h4 className="project-stack">Tech Stack - {project.node.stack}</h4>
-          {project.node.description && <p dangerouslySetInnerHTML={{ __html: project.node.description }} />}
+          <h4 className="project-stack">
+            <strong>Tech Stack</strong> - {project.node.stack}
+          </h4>
+          {project.node.description && (
+            <p className="project-description" dangerouslySetInnerHTML={{ __html: project.node.description }} />
+          )}
           <div>
             {project.node.source_link && (
               <a className="project-links" href={project.node.source_link}>
