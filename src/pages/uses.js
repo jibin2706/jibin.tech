cf016672547bbee9ce413cea732b351ff3a0cfa8
Jibin Thomas <jibin.thomas2706@gmail.com>
2020-09-20 15:21:20 +0530

    Update: uses page

--- src/pages/uses.js
+++ src/pages/uses.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import SEO from '../components/SEO'
 import Layout from '../components/layout'
+import System from '../images/overall.jpg'
 import '../styles/uses.scss'
+import { Link } from 'gatsby'
 
 function index() {
   return (
     <Layout>
       <SEO title="Uses" description="A page dedicated to the language/framework/tools and other stuff I use daily" />
 
-      <main className="uses mw-800 mx-auto">
+      <main className="uses mw-1200 mx-auto">
         <h1 className="uses-header">Things I use to get stuff done</h1>
 
         <div className="uses__grid">
+          <section className="uses-section pc">
+            <div className="uses-subsection">
+              <h2>My PC</h2>
+              <li>AOC 24B2XH 23.8</li>
+              <li>Ryzen 5 3600</li>
+              <li>Galax GTX 1650 Super</li>
+              <li>Asrock B450 SteelLegend Motherboard</li>
+              <li>Gskill Ripjaws 3200mhz (2 * 8gb) RAM</li>
+              <li>Corsair 110R cabinet</li>
+              <li>Deepcool Gammaxx GTE V2</li>
+              <Link to="/building-pc">Know more about the parts here</Link>
+            </div>
+            <img className="uses__image" src={System} alt="" />
+          </section>
           <section className="uses-section">
             <h2>Language</h2>
             <li>
@@ -41,16 +57,6 @@ function index() {
             </li>
             <li>
               <span className="uses-subheader">Flask -</span> Lately having my fair share of time working in backend
-            </li>
-          </section>
-
-          <section className="uses-section">
-            <h2>Gear</h2>
-            <li>
-              <span className="uses-subheader">Macbook Air -</span> Apple at work
-            </li>
-            <li>
-              <span className="uses-subheader">Acer Aspire 5 -</span> Windows at home
             </li>
           </section>
 
