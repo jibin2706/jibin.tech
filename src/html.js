349f5a7a0cd9a93f95affe16b5027fdc61494f59
Jibin Thomas <jibin.thomas2706@gmail.com>
2021-07-09 23:45:11 +0530

    Add meta viewport

--- src/html.js
+++ src/html.js
@@ -6,6 +6,9 @@ export default function HTML(props) {
     <html {...props.htmlAttributes} lang="en">
       <head>
         {props.headComponents}
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Source+Sans+Pro:wght@400;600;700&display=swap"
