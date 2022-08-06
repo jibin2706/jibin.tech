7ae3d340074494d44b903d03fb7914d1a614616a
Jibin Thomas <jibin.thomas2706@gmail.com>
2022-08-07 00:39:17 +0530

    Update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-postcss`,
-    `gatsby-transformer-json`,
     `gatsby-plugin-twitter`,
     // sitemap generation
     {
@@ -142,15 +141,6 @@ module.exports = {
               withWebp: true,
             },
           },
-          // adding id to headers in markdown
-          // {
-          //   resolve: `gatsby-remark-autolink-headers`,
-          //   options: {
-          //     maintainCase: false,
-          //     removeAccents: true,
-          //     icon: `<svg aria-hidden="true" width="24" height="24" viewBox="0 0 24 24"><path d="M22.548 9l.452-2h-5.364l1.364-6h-2l-1.364 6h-5l1.364-6h-2l-1.364 6h-6.184l-.452 2h6.182l-1.364 6h-5.36l-.458 2h5.364l-1.364 6h2l1.364-6h5l-1.364 6h2l1.364-6h6.185l.451-2h-6.182l1.364-6h5.366zm-8.73 6h-5l1.364-6h5l-1.364 6z"/></svg>`,
-          //   },
-          // },
           // code highlighting
           'gatsby-remark-code-titles',
           {
