d02b8bf7b784fbbd9480fbd08e4a003c119b4429
Jibin Thomas <jibin.thomas2706@gmail.com>
2022-08-07 00:26:33 +0530

    Remove gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -175,8 +175,5 @@ module.exports = {
         trickleSpeed: 400,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    'gatsby-plugin-offline',
   ],
 }
