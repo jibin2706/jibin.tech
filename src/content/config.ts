12d41dbc78d06747a80720d016123bfaceb520ee
Jibin Thomas <jibin.thomas2706@gmail.com>
2025-12-25 19:49:55 +0530

    blog listing page

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content'
+
+const blog = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		date: z.date(),
+		summary: z.string(),
+		tags: z.array(z.string()),
+		featured: z.boolean().optional(),
+		image: z.string().optional(),
+	}),
+})
+
+export const collections = { blog }
