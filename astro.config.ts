f0dbf807c95c09f18c21dbdecb0323b6f17fe5df
Jibin Thomas <jibin.thomas2706@gmail.com>
2025-12-27 11:22:37 +0530

    add metadata

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from '@astrojs/sitemap'
+import { SITE_URL } from './src/utils/constant'
 
 export default defineConfig({
+	site: SITE_URL,
+	integrations: [sitemap()],
 	vite: {
 		plugins: [tailwindcss()],
+	},
+	redirects: {
+		'/contact': '/info',
+		'/uses': '/info',
+		'/tags': '/blog',
 	},
 })
